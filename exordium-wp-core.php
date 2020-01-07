8f9bb60b2f703a98b1ea73d82d130ef58db20e7d
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-07 13:04:40 -0500

    main plugin files

---
+++ exordium-wp-core.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+Plugin Name: Exordium WP Core
+Plugin URI:
+Description: Adds exordium core functionality to wordpress and the exordium-wp-theme.
+Version: 1.0
+Author: Odyssey
+Author URI:
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+Text Domain: exordium-wp-core
+Domain Path: /plugin/languages
+*/
+
+/*
+Copyright 2009  Odyssey  (email : contact@exordium.dev)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+# initial plugin
+require_once( plugin_dir_path( __FILE__ ) . 'plugin/plugin.php' );
+
+?>
