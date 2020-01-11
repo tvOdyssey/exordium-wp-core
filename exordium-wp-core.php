480777784485a03b12d3f2c9e7994531e0920f55
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-11 18:18:12 -0500

    Update plugin information

--- exordium-wp-core.php
+++ exordium-wp-core.php
@@ -2,11 +2,11 @@
 
 /*
 Plugin Name: Exordium WP Core
-Plugin URI:
+Plugin URI: https://github.com/tvOdyssey/exordium-wp-core
 Description: Adds exordium core functionality to wordpress and the exordium-wp-theme.
 Version: 0.1
 Author: Odyssey
-Author URI:
+Author URI: https://github.com/tvOdyssey
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 Text Domain: exordium-wp-core
