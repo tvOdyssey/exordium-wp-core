133ef2e9f4c3552ffd6ca1f11cee4aa40101af98
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-16 13:56:23 -0500

    Completely redesign the plugin page

--- plugin/index/index.php
+++ plugin/index/index.php
@@ -16,12 +16,14 @@
   </head>
   <body>
 
-    <section class="main" style="margin-left: -20px;">
+    <main role="main">
 
-      <div class="jumbotron">
-        <div class="container-full">
+      <!-- jumbotron -->
+      <section class="jumbotron text-center bg-white" style="margin-left: -20px;">
+        <div class="container">
           <h1 class="display-4"><?php echo $info['name']; ?></h1>
-          <p class="lead">Documentation, and Changelog information on this wordpress plugin can be viewed on the Github repo.</p>
+          <p class="lead">Documentation, and Changelog information on this <br>
+            wordpress plugin can be viewed on the Github repo.</p>
           <p>
             <a href="https://github.com/tvOdyssey/exordium-wp-core" class="btn btn-dark">
               <i class="fab fa-github"></i> Exordium WP Core
@@ -32,74 +34,214 @@
           </p>
           <p class="text-muted f-s-11"><?php echo $info['name']; ?> is current running off of version <?php echo $info['version']; ?>.</p>
         </div>
-      </div>
+      </section>
 
-      <div class="container">
+      <!-- README.md -->
+      <section class="py-4 bg-light" style="margin-left: -20px;">
 
-        <div class="row">
-          <div class="col-md-12">
+        <div class="container">
 
-            <div class="mt-5">
-              <h4>Plugin Readme</h4>
-              <p><code>exordium-wp-core</code> is a customized wordpress plugin made for the <a href="https://exordium.dev">https://exordium.dev</a> website. Exordium is a virtual private networking software in the works.</p>
-              <p>This plugin has been released open source and will have features required for the Exordium website, including but not limited to the following features.</p>
-            </div>
-
-            <div class="mt-5">
-              <h4>Functionality</h4>
-              <p>You can enable or disable features of this plugin you don't want to have access to. By default, all extensions of this plugin are enabled.</p>
-
-              <!-- subtitle -->
-              <div class="card w-100" style="padding:0;">
-                <div class="card-body">
-                  <h5 class="card-title">Page/Post Subtitles</h5>
-                  <p class="card-text">This feature allows you to create subtitles for your pages and posts. If you're using a theme that isn't the <code>exordium-wp-theme</code> you'll need to add those abilities into your themes <code>function.php</code> file.</p>
-
-                  <form class="form">
-                    <div class="form-group">
-                      <div class="custom-control custom-switch">
-                        <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                        <label class="custom-control-label" for="customSwitch1">Subtitle functionality</label>
-                      </div>
-                    </div>
-
-                    <div class="form-group">
-                      <a href="#" class="btn btn-primary">Update feature</a>
-                    </div>
-                  </form>
-
+          <!-- card -->
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title">
+                Subtitles
+              </h5>
+              <p class="card-text">
+                This feature allows you to create subtitles for your pages and posts. If you're using a theme that isn't the <code>exordium-wp-theme</code> you'll need to add those abilities into your themes <code>function.php</code> file.
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-switch">
+                  <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                  <label class="custom-control-label" for="customSwitch1">Enable/Disabled</label>
                 </div>
-              </div>
-
-              <!-- avatars -->
-              <div class="card w-100" style="padding:0;">
-                <div class="card-body">
-                  <h5 class="card-title">Uploadable Avatars</h5>
-                  <p class="card-text">Allows members with access to the dashboard to upload their own avatars, and also use generated ones. </p>
-
-                  <form class="form">
-                    <div class="form-group">
-                      <div class="custom-control custom-switch">
-                        <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                        <label class="custom-control-label" for="customSwitch1">Subtitle functionality</label>
-                      </div>
-                    </div>
-
-                    <div class="form-group">
-                      <a href="#" class="btn btn-primary">Update feature</a>
-                    </div>
-                  </form>
-
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-pen"></i>
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-cogs"></i>
+                    Settings
+                  </button>
                 </div>
               </div>
             </div>
-
           </div>
+
+          <!-- card -->
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title">
+                Avatars
+              </h5>
+              <p class="card-text">
+                Allows members with access to the dashboard to upload their own avatars, and also use generated ones.
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-switch">
+                  <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                  <label class="custom-control-label" for="customSwitch1">Enable/Disabled</label>
+                </div>
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-pen"></i>
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-cogs"></i>
+                    Settings
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- card -->
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title">
+                Exordium Forms
+              </h5>
+              <p class="card-text">
+                Allows members with access to the dashboard to upload their own avatars, and also use generated ones.
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-switch">
+                  <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                  <label class="custom-control-label" for="customSwitch1">Enable/Disabled</label>
+                </div>
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-pen"></i>
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-cogs"></i>
+                    Settings
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- card -->
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title">
+                Category Featured Images
+              </h5>
+              <p class="card-text">
+                Allows members with access to the dashboard to upload their own avatars, and also use generated ones.
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-switch">
+                  <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                  <label class="custom-control-label" for="customSwitch1">Enable/Disabled</label>
+                </div>
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-pen"></i>
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-cogs"></i>
+                    Settings
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- card -->
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title">
+                Activity Log
+              </h5>
+              <p class="card-text">
+                Allows members with access to the dashboard to upload their own avatars, and also use generated ones.
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-switch">
+                  <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                  <label class="custom-control-label" for="customSwitch1">Enable/Disabled</label>
+                </div>
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-pen"></i>
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-cogs"></i>
+                    Settings
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- card -->
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title">
+                Realtime Statistics
+              </h5>
+              <p class="card-text">
+                Allows members with access to the dashboard to upload their own avatars, and also use generated ones.
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-switch">
+                  <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                  <label class="custom-control-label" for="customSwitch1">Enable/Disabled</label>
+                </div>
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-pen"></i>
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-cogs"></i>
+                    Settings
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- card -->
+          <div class="card mb-4 shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title">
+                Dashboard Widgets
+              </h5>
+              <p class="card-text">
+                Allows members with access to the dashboard to upload their own avatars, and also use generated ones.
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-switch">
+                  <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                  <label class="custom-control-label" for="customSwitch1">Enable/Disabled</label>
+                </div>
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-pen"></i>
+                    Update
+                  </button>
+                  <button type="button" class="btn btn-sm btn-outline-dark">
+                    <i class="fa fa-fw fa-cogs"></i>
+                    Settings
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
 
-      </div>
+      </section>
 
-    </section>
+    </main>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
